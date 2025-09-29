ffd17d119cab26ebb5512c9c2862f4246ad201b9
ASHISHSE <143174684+ASHISHSE@users.noreply.github.com>
2025-09-29 11:39:00 +0530

    Update App_10_F(added NDVI,NDWI, MAI)_F_chart_v2.py

--- App_10_F(added NDVI,NDWI, MAI)_F_chart_v2.py
+++ App_10_F(added NDVI,NDWI, MAI)_F_chart_v2.py
@@ -18,7 +18,7 @@ st.set_page_config(page_title="🌱 Crop Advisory System", page_icon="🌱", lay
 @st.cache_data
 def load_data():    
     weather_url = "https://github.com/ASHISHSE/App_test/raw/main/weather.xlsb"
-    rules_url = "https://github.com/ASHISHSE/App_test/raw/main/rules.xlsx"
+    rules_url = "https://github.com/ASHISHSE/App_test/raw/main/rules - Copy_F.xlsx"
     sowing_url = "https://github.com/ASHISHSE/App_test/raw/main/sowing_calendar1.xlsx"
 
     try:
@@ -908,6 +908,7 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
+
 
 
 
